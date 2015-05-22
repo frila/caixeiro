6a0aa7183bf1e6d2149c36edb285b16c001e3e95
Bernardo Lins <bernardolins28@gmail.com>
2015-05-21 23:33:25 -0300

    Add initialization to tour.

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include "tour.h"
 
+#define NCITIES 5
+
 int main() {
+	tour t = create_tour(NCITIES);
+
+	int i;
+	for (i = 0; i < NCITIES; i++)
+	{
+		printf("%d\n", t[i]);
+	}
 
 	printf("Hello!");
 
