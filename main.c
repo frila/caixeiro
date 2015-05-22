cea64f7221398e71fcbcc60e0572a7bc7594a1c2
Bernardo Lins <bernardolins28@gmail.com>
2015-05-22 00:15:46 -0300

    Test all functions from tour.h.

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
 #include "tour.h"
 
 #define NCITIES 5
+#define STARTPOINT 2
 
 int main() {
 	tour t = create_tour(NCITIES);
+	populate_tour(t, NCITIES);
+	start_point(t, STARTPOINT);
+	swap_cities(t, 3, 4);
 
 	int i;
 	for (i = 0; i < NCITIES; i++)
@@ -11,7 +15,8 @@ int main() {
 		printf("%d\n", t[i]);
 	}
 
-	printf("Hello!");
+
+	printf("Hello!\n");
 
     return 0;
 }
