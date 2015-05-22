420d9ccc427be90c400d7774e386ef3f9e133a80
Bernardo Lins <bernardolins28@gmail.com>
2015-05-21 21:34:37 -0300

    Yet some refactoring.

--- example/example.c
+++ example/example.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <ctype.h>
+#include <string.h>
 
 void troca(int* valor1, int* valor2) {
 	int tmp = *valor1;
@@ -9,15 +10,38 @@ void troca(int* valor1, int* valor2) {
 	*valor2 = tmp;
 }
 
+void print_matriz(int **m, int n){
+	int i,j;
+	for(i = 0; i < n; i++){
+		for(j = 0; j < n; j++)
+			printf("%d ", m[i][j]);
+		printf("\n");
+	}
+}
+
+int **le_entrada(int n) {
+	int i,j;
+	int *vetor_alocado = (int*) malloc (sizeof(int) * n * n);
+	int **vetor = (int**) malloc (sizeof(int*) * n);
+
+	for(i = 0; i < n; i++){
+		vetor[i] = vetor_alocado + i * n ;
+		for(j = 0; j < n; j++)
+			scanf("%d", &vetor[i][j]);
+	}
+
+	return vetor;
+} 
+
+
 int main(int argc, char *argv[]) {
-	int* caminho;
+	int* caminho, *caminho_resposta;
 	int cidade_inicial = 0, ncidades = 0;
-	int peso[4][4] = { {999,3,5,4}, {3,999,8,6}, {5,8,999, 1}, {4, 6, 1, 999} };
+	int **peso;
 	int menor_peso = 999;
 	
 	int i = 1, j, k;
 
-	int* caminho_resposta = caminho;
 
 	char a;
 	while((a = getopt(argc, argv, "n:s:")) != -1) {
@@ -27,22 +51,29 @@ int main(int argc, char *argv[]) {
 		}
 	}
 
+	peso = le_entrada(ncidades);
+	print_matriz(peso,ncidades);
+	// exit(1);
+
 	//refatorar isso aqui
 	caminho = (int*)malloc(sizeof(int)*ncidades);
+	caminho_resposta = (int*)malloc(sizeof(int)*ncidades);
+
 	for(j = 0; j < ncidades; j++) caminho[j] = j;
 
 	caminho[0] = cidade_inicial;
 	caminho[cidade_inicial] = 0;
 
-	while (i < ncidades) {
+	for (i = 1; i < ncidades; i++) {
 		for(j = 1; j < ncidades; j++) j != ncidades - 1 ? troca(&caminho[j], &caminho[j+1]) : troca(&caminho[j], &caminho[1]);
 
 		int peso_caminho = 0;
-		for(k = 0; k < ncidades; k++) k != ncidades -1 ? (peso_caminho += peso[caminho[k]][caminho[k+1]]) : (peso_caminho += peso[caminho[k]][caminho[0]]);
+		for(k = 0; k < ncidades; k++) k != ncidades - 1 ? (peso_caminho += peso[caminho[k]][caminho[k+1]]) : (peso_caminho += peso[caminho[k]][caminho[0]]);
 
-		if (peso_caminho < menor_peso) { menor_peso = peso_caminho; caminho_resposta = caminho; }
-
-		i++;
+		if (peso_caminho < menor_peso) { 
+			menor_peso = peso_caminho;
+			memcpy(caminho_resposta, caminho, sizeof(int) * ncidades);
+		}
 	}
 
 	printf("menor peso: %d\n", menor_peso);
@@ -50,6 +81,7 @@ int main(int argc, char *argv[]) {
 	printf("caminho resposta: [ ");
 	for(k = 0; k < ncidades; k++) {
 		printf("%d ", caminho_resposta[k]);
+
 	}
-	printf("]\n");
+	printf("%d ]\n", cidade_inicial);
 }
