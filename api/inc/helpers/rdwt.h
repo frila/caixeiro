038b1e9d00637bd01121124420dba2dbc215c17e
Fausto Ferreira Junqueira <faustoferjunqueira@gmail.com>
2015-05-22 01:31:39 -0300

    Move includes to .c

--- api/inc/helpers/rdwt.h
+++ api/inc/helpers/rdwt.h
@@ -6,16 +6,14 @@
 #define CAIXEIRO_RDWT_H
 
 #include "tour.h"
-#include <unistd.h>
-#include <ctype.h>
-#include <string.h>
-
 
 // Lê as distancias entre as cidades da entrada padrão
 distance_table rd_distance(int ncities);
 
 // Print o melhor caminho e o custo
 void pt_betterpath(tour t, city start_city, int ncities);
+
+// Read parameters to terminal
 void get_input(int argc, char* argv[], int* ncities, int* start_city);
 
 #endif //CAIXEIRO_RDWT_H
