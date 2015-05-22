eac2cd7cffef14d419bf483e0d655d2f2bce4a4c
Bernardo Lins <bernardolins28@gmail.com>
2015-05-22 02:36:48 -0300

    Implement algorithm.

--- main-parallel.c
+++ main-parallel.c
@@ -1,22 +1,47 @@
 #include "tour.h"
+#include "rdwt.h"
+#include "def.h"
+#include <string.h>
 
-#define NCITIES 5
-#define STARTPOINT 2
+int main(int argc, char *argv[]) {
+    //definindo variáveis auxiliares
+    int ncities, start_city;
+    distance_table distances;
+    distance shorter_distance = INF;
+    
+    int i, j;
 
-int main() {
-	tour t = create_tour(NCITIES);
-	populate_tour(t, NCITIES);
-	start_point(t, STARTPOINT);
-	swap_cities(t, 3, 4);
+    //lendo input: número de cidades e cidade inicial
+    get_input(argc, argv, &ncities, &start_city);
+    //lendo input: matriz de distancias
+    distances = rd_distance(ncities);
 
-	int i;
-	for (i = 0; i < NCITIES; i++)
-	{
-		printf("%d\n", t[i]);
-	}
+    //inicializando tour
+    tour t = create_tour(ncities);
+    populate_tour(t, ncities);
+    start_point(t, start_city);
 
+    //inicializando o tour que vai guardar o melhor caminho
+    tour better_tour = create_tour(ncities);
 
-	printf("Hello!\n");
+    //executando o algoritmo de permutação e comparando os pesos dos caminhos
+	for(i = 1; i < ncities; i++) {
+		for(j = 1; j < ncities; j++) { 
+            if(j != ncities - 1) {
+                swap_cities(t, j, j+1);
+            } else {
+                swap_cities(t, j, 1);
+            }
+        }
+
+        distance sh;
+        if((sh = is_shorter(t, distances, ncities, shorter_distance))) {
+            shorter_distance = sh;
+            memcpy(better_tour, t, sizeof(city)*ncities);
+        }
+    }
+
+    pt_betterpath(better_tour, start_city, ncities, shorter_distance);
 
     return 0;
 }
