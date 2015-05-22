ff10d44186b174699da9105fac85be89ef5b95c3
Bernardo Lins <bernardolins28@gmail.com>
2015-05-21 23:32:32 -0300

    Add create_tour function.

--- api/src/tour.c
+++ api/src/tour.c
@@ -1,5 +1,10 @@
 #include "tour.h"
 
+tour create_tour(int ncities) {
+	tour t = (int*)malloc(sizeof(int)*ncities);
+	return t;
+}
+
 int is_shtr(tour t, int dist) { return 1; }
 
 void strt_point(tour t, city c) {}
