a26c061ac7d81ae69345516fe29c96e6ebc2f7d5
Bernardo Lins <bernardolins28@gmail.com>
2015-05-22 00:13:37 -0300

    Create populate tour function.

--- api/src/tour.c
+++ api/src/tour.c
@@ -2,11 +2,30 @@
 
 tour create_tour(int ncities) {
 	tour t = (int*)malloc(sizeof(int)*ncities);
+	
 	return t;
 }
 
-int is_shtr(tour t, int dist) { return 1; }
+void populate_tour(tour t, int ncities) {
+	int i;
+	for (i = 0; i < ncities; i++) {
+		t[i] = i;
+	}
+}
 
-void strt_point(tour t, city c) {}
+int is_shorter(tour t, int dist) { 
+	// int current_dist = 0;
 
-void swap_cities(tour* t, city c1, city c2) {}
+	return 1;
+}
+
+void start_point(tour t, city c) {
+	t[0] = c;
+	t[c] = 0;
+}
+
+void swap_cities(tour t, city c1, city c2) {
+	city tmp = t[c1];
+	t[c1] = t[c2];
+	t[c2] = tmp; 
+}
